7249f8feb9fbd3986bd45718bac4ab84d67db7c4
Ira Shoemo <irashoemo1@gmail.com>
2024-06-12 17:31:37 -0400

    changed app keys in server.js

--- server/config/server.js
+++ server/config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: [
+      "eRx1eIFP/RWlsRSrWKvnlg==",
+      "jn9UFihP93d8klX9FW3BkA==",
+      "w4GZR+Mh2qyYfGfJvGJqHQ==",
+      "c8ikerPvG5VQUf8tcMfFZQ==",
+    ],
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });

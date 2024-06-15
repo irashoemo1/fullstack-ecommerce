0a4c70a9dd13d15fe63f8446b5fd3d29147feed3
Ira Shoemo <irashoemo1@gmail.com>
2024-06-15 12:24:27 -0400

    port

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 1337) || 1337,
   app: {
     keys: [
       "eRx1eIFP/RWlsRSrWKvnlg==",
